5f0ef78032d515b28efd949687c8d8f7e315c6fe
Pavelman3321 <Ostapenko3321@gmail.com>
2019-05-27 22:34:49 +0700

    new

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 'use strict'
 
+let answer = prompt("какой у вас бюджет");
+console.log(answer);
+
+let timeData = prompt("укажите дату в стиле","ДД-ММ-ГГ");
+console.log(timeData);
+
+let appData = [answer,timeData];
+//console.log(appData[1]);
+
+let need = newFunction();
+
+let hm = promt("Во сколько обойдется?");
+
+function newFunction() {
+    return promt("Введите обязательную статью расходов в этом месяце");
+}
+
